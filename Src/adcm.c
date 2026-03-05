0f9f8762f353990ab6a180f718109273988516dd
PrajwalHegde <117985355+Starscrouge@users.noreply.github.com>
2026-03-05 10:31:28 +0530

    Update adcm.c

--- Src/adcm.c
+++ Src/adcm.c
@@ -1,15 +1,7 @@
 #include <stdint.h>
 #include "adcm.h"
 #include "stm32f4xx.h"
-#define GPIOAEN (1U<<0)
-#define ADCEN (1U<<8)
-#define PIN1 (1U<<1)
-#define ADC_CHAN (1U<<0)
-#define SEQ_ADC 0x00;
-#define ADC_ON (1U<<0)
-#define SOFT_START (1U<<30)
-#define SR_EOC (1U<<1)
-#define CR1_EOCIE (1U<<5)
+
 void pa1_adc_init(void){
 	RCC->AHB1ENR |= GPIOAEN;
 	GPIOA->MODER|= (1U<<2);
