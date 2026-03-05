46d19321e572ab4690675c7d30f429372091f88d
PrajwalHegde <117985355+Starscrouge@users.noreply.github.com>
2026-03-05 10:31:51 +0530

    Update adcm.h

--- Inc/adcm.h
+++ Inc/adcm.h
@@ -1,4 +1,14 @@
 #include<stdint.h>
+
+#define GPIOAEN (1U<<0)
+#define ADCEN (1U<<8)
+#define PIN1 (1U<<1)
+#define ADC_CHAN (1U<<0)
+#define SEQ_ADC 0x00;
+#define ADC_ON (1U<<0)
+#define SOFT_START (1U<<30)
+#define SR_EOC (1U<<1)
+#define CR1_EOCIE (1U<<5)
 void pa1_adc_init(void);
 void start_conv(void);
 uint32_t adc_read(void);
